1543e7331704ffa062541c2b7809e193598cb639
weiyan <pink040766@gmail.com>
2024-03-31 21:46:38 +0800

    加入專案檔案。

---
+++ OXGame/Form1.cs
@@ -0,0 +1,15 @@
+namespace OXGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void O(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
